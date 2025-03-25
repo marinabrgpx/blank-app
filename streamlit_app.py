cef9153f5879da3ea94bbf0d706afba17c2b01ae
marinabrgpx <mbatllo@globalpraxis.com>
2025-03-25 09:57:35 +0000

    requirements_update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
+import functions as f
+import pandas as pd
 
-st.title("🎈 My new Streamlit app")
-st.write(
-    "This is a trial to see how it is working."
-)
+uploaded_file = st.sidebar.file_uploader("Load Data", type = ["xlsx"])
+df = pd.read_excel(uploaded_file, sheet_name="actual_model")
+st.dataframe(df, use_container_width=True)
